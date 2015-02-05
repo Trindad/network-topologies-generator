9931c6dd5a9dabb34cad1935eda1d96f6dc7a566
Silvana Trindade <syletri@gmail.com>
2015-02-05 09:45:58 -0200

    Corrigindo bugs nas ligações

---
+++ src/lib/Brandes.hpp
@@ -0,0 +1,21 @@
+/**
+ * Definições de função-membro que aparecem em Dijksta.cpp
+ */
+#include <iostream>
+#include <deque>
+#include <iterator>
+#include <algorithm> 
+#include <stdexcept> // for std::runtime_error
+#include <vector>
+#include <random>
+
+
+ using namespace std;
+
+ class Brandes
+ {
+ public:
+ 	Brandes();
+ 	~Brandes();
+ 	
+ };
---
+++ src/lib/Measure.hpp
@@ -0,0 +1,21 @@
+/**
+ * Definições de função-membro que aparecem em Measure.cpp
+ */
+#include <iostream>
+#include <deque>
+#include <iterator>
+#include <algorithm> 
+#include <stdexcept> // for std::runtime_error
+#include <vector>
+#include <random>
+
+
+ using namespace std;
+
+ class Measure
+ {
+ public:
+ 	Measure();
+ 	~Measure();
+ 	
+ };
