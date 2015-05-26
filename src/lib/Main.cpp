36209433b3c614d387f344aa20e5f5cb80257973
Silvana Trindade <syletri@gmail.com>
2015-05-26 17:16:18 -0300

    Lendo topologia

--- src/lib/Main.cpp
+++ src/lib/Main.cpp
@@ -1,5 +1,10 @@
 #include "Graph.hpp"
 #include "Suurballe.hpp"
+#include <sstream>
+
+vector<string> &split(const string &s, char delim, vector<string> &elems);
+
+vector<string> split(const string &s, char delim);
 
 int main(int argc, char const *argv[])
 {
@@ -11,25 +16,56 @@ int main(int argc, char const *argv[])
 	ifstream file;
 
 	file.open(argv[1]);
+
 	
 	if (file.is_open())
 	{
+		Graph g;
+
 		string line = " ";
 
 		getline (file,line);
 
 		int n = stoi(line);//obtêm o número de nós
 
+		g.setNumberOfNodes(n);
+		g.setMinimumDegree(2);
+		g.setMaximumDegree(n-1);
+
 		cout<<"Número de nós: "<<n<<endl;
+		vector<string> nodes;
 
 		while( getline (file,line) )
 		{
-			cout<<" "<<line<<endl;
+			nodes = split(line.c_str(),' ');
+			cout<<" "<<nodes[0]<<" "<<nodes[1]<<endl;
+
+			int u = stoi(nodes[0])-1;
+			int v = stoi(nodes[1])-1;
+			g.setEdge(u,v);
 		}
+
 		file.close();
 
 	}
 
 	
 	return 0;
+}
+
+
+vector<string> &split(const string &s, char delim, vector<string> &elems) {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+
+vector<string> split(const string &s, char delim) {
+   vector<string> elems;
+    split(s, delim, elems);
+    return elems;
 }
